744c9c96a9ca20f222483a1454b7e1e52fa5a914
woz07 <105782312+woz07@users.noreply.github.com>
2025-04-16 17:46:04 +0100

    Add files via upload

---
+++ src/main/java/github/woz07/Main.java
@@ -0,0 +1,142 @@
+package github.woz07;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+    private static final Set<String> PLATES = new HashSet<>();  // Data structure to stores plates
+    private static final String FILE        = "plates.txt";     // The file which stores the plates that get loaded and saved to
+    private static final char[] LETTERS     = {                 // The alphabet in char array that gets accessed when generating plate
+                'A','B','C','D','E','F','G','H','I','J','K',
+                'L','M','N','O','P','Q','R','S','T','U','V',
+                'W','X','Y','Z'
+        };
+    private static final Random RANDOM = new Random();          // The randomizer, used in generating plates
+
+    // Entry point;
+    // Loads plates, generates plates and saves them
+    public static void main(String[] args) {
+
+        /*
+         * Assumptions:
+         * - A folder is created where "epgt.exe" is put and then ran from terminal
+         * - Java 11 (Amazon Corretto 11.0.24) is installed and used to run the jar, if running the jar
+         * - The program is being run on Windows 11
+         * - Program is executed from the root directory where "plates.txt" resides
+         * - Content within "plates.txt" is in plaintext format, one plate per line
+         * - No duplicate memory tags are passed in during runtime
+         * - All input date strings follow "dd/MM/yyyy" format
+         * - The sizes of memorytags and datetags should be equal ((memorytags.size() == datetags.size()) -> true)
+         * - No memory tags contain invalid characters (e.g., numbers or special symbols)
+         * - Program has full permissions to read/write/delete "plates.txt" from AppData/Roaming/epgt/
+         * - Program runs in a single threaded context (no parallel execution)
+        */
+
+        // Load previously stored plates from file to set
+        LoadGeneratedPlates();
+
+        // Hard coded test data
+        String[] memorytags = new String[]{"YC", "LT", "FF"};
+        String[] datetags   = new String[]{"04/07/2019", "23/01/2003", "30/05/2032"};
+
+        // Generate plates
+        // ASSUME memorytags.size() == datetags.size()
+        for (int i = 0; i < memorytags.length; i++) {
+            String plate = GeneratePlate(memorytags[i], datetags[i]);
+            System.out.println((i + 1) + ") Generated Plate: " + plate);
+        }
+
+        // Save plates from set to file
+        SaveGeneratedPlates();
+    }
+
+    /**
+     * Generate unique plate using provided parameters (memory tag and date tag)
+     * @param memorytag The 2 letters that are used for memory identification
+     * @param datetag   The date used for the plate to generate age identifier
+     * @return          The unique generated plate
+     */
+    public static String GeneratePlate(String memorytag, String datetag) {
+        LocalDate date = LocalDate.parse(datetag, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+        int month = date.getMonthValue();
+        int year  = date.getYear();
+
+        // March = 3rd month, August = 9th month
+        // If `month` not between 3rd-8th then add 50 but also minus 1 from year
+        // else don't add 50
+        int age;
+        if (month >= 3 && month <= 8)
+            age = year % 100;
+        else
+            age = ((year - 1) % 100) + 50;
+
+        String letters;
+        String plate;
+        do {
+            letters = GetRandomLetters(3, true);
+            plate   = memorytag + String.format("%02d", age) + letters;
+        } while (PLATES.contains(plate));
+
+        PLATES.add(plate);
+
+        return plate;
+    }
+
+    /**
+     * Returns a string of count `length` random letters (including both 'I' and 'Q')
+     * @param length    How many random letters
+     * @param exclude   Should the generation exclude 'I' and 'Q'? True == yes, False == no
+     * @return          Returns a string of random generated letters of length `n`
+     */
+    public static String GetRandomLetters(int length, boolean exclude) {
+        StringBuilder letters = new StringBuilder(length);
+
+        for (int i = 0; i < length; ++i) {
+            char letter = LETTERS[RANDOM.nextInt(LETTERS.length)];
+            if (exclude && (letter == 'I' || letter == 'Q')) {
+                i--;
+            } else {
+                letters.append(letter);
+            }
+        }
+
+        return letters.toString();
+    }
+
+    /**
+     * Loads previously generated plates from file into set `PLATES`
+     */
+    private static void LoadGeneratedPlates() {
+        File file = new File(FILE);
+
+        if (!file.exists()) {
+            // No saved files exists to read from yet
+            return;
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                PLATES.add(line.trim());
+            }
+        } catch (IOException err) {
+            System.err.println("Plates could not be loaded: " + err.getMessage());
+        }
+    }
+
+    /**
+     * Writes current plates within set `PLATES` to file of path `FILE`
+     */
+    private static void SaveGeneratedPlates() {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE, false))) {
+            for (String plate : PLATES) {
+                writer.println(plate);
+            }
+        } catch (IOException err) {
+            System.err.println("Plates could not be saved: " + err.getMessage());
+        }
+    }
+}
---
+++ src/test/java/github/woz07/MainTest.java
@@ -0,0 +1,157 @@
+package github.woz07;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    // Note: `max` in this context is the same as expected
+
+    @org.junit.jupiter.api.Test
+    void testPattern() {
+        // Passes
+        String[] arr = {"YC19ZCT"};
+
+        for (String pass : arr) {
+            System.out.println(pass.matches("^[A-Z]{2}\\d{2,3}[A-HJ-NPR-Z|]{3}$") ? "Pass passed [GOOD]" : "Pass failed [BAD]");
+        }
+
+        // Fails
+        arr = new String[]{"YC19ZCI"};
+
+        for (String fail : arr) {
+            System.out.println(fail.matches("^[A-Z]{2}\\d{2,3}[A-HJ-NPR-Z|]{3}$") ? "Fail passed [BAD]" : "Fail failed [GOOD]");
+        }
+    }
+
+    /**
+     * General testing to make sure that GeneratePlate generates correct plates
+     */
+    @org.junit.jupiter.api.Test
+    void testGeneratePlates() {
+        // If you would like to test an even greater amount just alter 'max' to a higher value
+        // Note: Doing so may take a longer time and slow down computer, depending on value set for max
+        // Warning: If you set max to Integer.MAX_VALUE in this context then you will most likely run into an OutOfMemoryError exception "Java heap space"
+
+        int passed = 0;
+        int failed = 0;
+        int max    = 1000000;
+
+        // Generate a bunch of memory tags
+        List<String> memorytags = new ArrayList<>();
+        for (int i = 0; i < max; i++) {
+            memorytags.add(Main.GetRandomLetters(2, false));
+        }
+
+        // Generate a bunch of date tags
+        List<String> datetags       = new ArrayList<>();
+        Random random               = new Random();
+
+        for (int i = 0; i < max; i++) {
+            int year = random.nextInt(2100 - 1900) + 1900; // Testing from 1900 to 2099
+            int month = random.nextInt(12) + 1; // 1-12 (Jan - Dec)
+            int day;
+
+            // Handle varying month lengths
+            switch (month) {
+                case 2: // 28 days
+                    day = random.nextInt(28) + 1;
+                    break;
+                case 4: case 6: case 9: case 11: // 30 days
+                    day = random.nextInt(30) + 1;
+                    break;
+                default: // 31 days
+                    day = random.nextInt(31) + 1;
+            }
+
+            String datestring = String.format("%02d/%02d/%04d", day, month, year);
+            datetags.add(datestring);
+        }
+
+        for (int i = 0; i < max; i++) {
+            try {
+                String plate = Main.GeneratePlate(memorytags.get(i), datetags.get(i));
+                // NOTE: This allows for plates of length 8 (where the age identifier has gone above 100)
+                if (plate.matches("^[A-Z]{2}\\d{2,3}[A-HJ-NPR-Z|]{3}$"))
+                    passed++;
+                else
+                    failed++;
+            } catch (Exception e) {
+                failed++;
+            }
+        }
+
+        System.out.println("GeneratePlates() test result:");
+        System.out.println("Max: " + max);
+        System.out.println("Passed: " + passed);
+        System.out.println("Failed: " + failed);
+
+        assertEquals(max, passed);
+    }
+
+    /**
+     * General testing to make sure that `GetRandomLetters` for memory tag puts any letter,
+     * regardless if 'I' or 'Q' and is exactly length of 2
+     */
+    @org.junit.jupiter.api.Test
+    void testGetRandomLettersForMemoryTag() {
+        // If you would like to test an even greater amount just alter 'max' to a higher value
+        // Note: Doing so may take a longer time and slow down computer, depending on value set for max
+
+        int passed = 0;
+        int failed = 0;
+        int max    = 100000;
+
+        String value;
+        for (int i = 0; i < max; i++) {
+            value = Main.GetRandomLetters(2, false);
+
+            // Since it's hard to test this we are just going to test for capital A-Z and exactly 2 letters
+            if (value.length() == 2)
+                passed++;
+            else
+                failed++;
+        }
+
+        System.out.println("GetRandomLettersForMemoryTag() Result:");
+        System.out.println("Max: " + max);
+        System.out.println("Passed: " + passed);
+        System.out.println("Failed: " + failed);
+
+        assertEquals(max, passed);
+    }
+
+    /**
+     * General testing to make sure that `GetRandomLetters` for random letter
+     * doesn't return any 'I' or 'Q' and is exactly length of 3
+     */
+    @org.junit.jupiter.api.Test
+    void testGetRandomLettersForRandomLetters() {
+        // If you would like to test an even greater amount just alter 'max' to a higher value
+        // Note: Doing so may take a longer time and slow down computer, depending on value set for max
+
+        int passed = 0;
+        int failed = 0;
+        int max    = 100000;
+
+        String value;
+        for (int i = 0; i < max; i++) {
+            value = Main.GetRandomLetters(3, true);
+
+            if (!value.contains("I") && !value.contains("Q") && value.length() == 3)
+                passed++;
+            else
+                failed++;
+        }
+
+        System.out.println("GetRandomLettersForRandomLetters() Result:");
+        System.out.println("Max: " + max);
+        System.out.println("Passed: " + passed);
+        System.out.println("Failed: " + failed);
+
+        assertEquals(max, passed);
+    }
+}
